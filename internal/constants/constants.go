c04f8133e3f1a35120902270deca7253645cf26e
Tanya Ten <izvestiya9@mail.ru>
2024-11-15 14:12:42 +0300

    fix

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -2,3 +2,4 @@ package constants
 
 const UserIDKey = "userID"
 const JWTSecret = "supersecretkey"
+const TestToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzE3NTM2NDAsInVzZXJfaWQiOjZ9.M24Qi4AxzHeDaLVv8z8Ww9J4Y4p8OdgpnwCE6BDNBM4"
