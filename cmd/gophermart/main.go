d48dd9a5cf8afd8e89fdbe4e684ff963d4d67433
Tanya Ten <izvestiya9@mail.ru>
2024-11-15 12:02:19 +0300

    fix

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	// "os/exec"
 
 	logger "github.com/thalq/gopher_mart/internal/middleware"
 	"github.com/thalq/gopher_mart/pkg/config"
@@ -16,9 +17,17 @@ func main() {
 	storage.InitDB(cfg.DatabaseURI)
 	router := router.NewRouter(cfg)
 
+	// cmd := exec.Command("./accrual_darwin_arm64", cfg.AccrualSystemAddress)
+	// output, err := cmd.CombinedOutput()
+
+	// if err != nil {
+	// 	logger.Sugar.Fatalf("Ошибка запуска исполняемого файла: %s", err)
+	// 	return
+	// }
+	// logger.Sugar.Infof("Исполняемы файл запущен %s", string(output))
+
 	logger.Sugar.Infof("Starting server on %s", cfg.RunAdress)
 	if err := http.ListenAndServe(cfg.RunAdress, router); err != nil {
 		logger.Sugar.Fatalf("Error run server: %s", err)
 	}
-
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -23,7 +23,7 @@ func getEnv(value string, defaultValue string) string {
 func NewConfig() *Config {
 	defaultRunAdress := "localhost:8080"
 	defaultDatabaseURI := "postgres://postgres:postgres@localhost/postgres?sslmode=disable"
-	defaultAccrualAddress := "/Users/tanya/go/src/github.com/thalq/gopher_mart/cmd/accrual/accrual_darwin_arm64"
+	defaultAccrualAddress := "localhost:8081"
 	envRunAddress := getEnv("RUN_ADDRESS", defaultRunAdress)
 	envDatabaseURI := getEnv("DATABASE_URI", defaultDatabaseURI)
 	envAccrualSystemAddress := getEnv("ACCRUAL_SYSTEM_ADDRESS", defaultAccrualAddress)
